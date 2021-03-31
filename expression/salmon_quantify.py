6cf5108231180fb9658da050178bf01d100e6696
yduanBioinfo <duanyou@outlook.com>
2021-04-01 02:11:50 +0800

    Remove comments codes.

--- expression/salmon_quantify.py
+++ expression/salmon_quantify.py
@@ -20,22 +20,12 @@ def run_salmonpip(myfq1,myfq2,fqnames,genome, gff, outdir, p=8):
     for i in range(len(myfq1)):
         args.extend((fqnames[i],myfq1[i],myfq2[i]))
     order = " ".join(map(str,args))
-    #os.system(order)
     subprocess.call([order],shell=True)
 
 def salmonpip(conf,myfq1,myfq2,fqnames,outdir,gff,p=8):
     """ Function for run_RNAseqpipe """
     genome = conf["all"]["genome"]
     run_salmonpip(myfq1,myfq2,fqnames,genome,gff,outdir,p)
-
-def salmonpip_bk(conf,myfq1,myfq2,fqnames,outdir,gff,p=8):
-    #run salmon
-    args = [SALMON,gff,conf["all"]["genome"],outdir,p]
-    for i in range(len(myfq1)):
-        args.extend((fqnames[i],myfq1[i],myfq2[i]))
-    order = " ".join(map(str,args))
-    #os.system(order)
-    subprocess.call([order],shell=True)
 
 def main(argv):
     import argparse
@@ -49,24 +39,18 @@ def main(argv):
     parser.add_argument('-t', '--thread', default=8, type=int,\
         help="Number of thread used for salmon")
     parser.add_argument('-g','--group_data',help='group_data file. conflict with -1 -2',nargs='?')
-    #parser.add_argument('-c','--conf',required=True,help="conf file")
-    #parser.add_argument('-c','--conf',help="conf file")
     parser.add_argument('-o','--outdir',required=True,help="output directory")
     args = parser.parse_args(argv[1:])
+
     ## set to info level
     log.setLevel(20)
-    #myconf = Configuration(args.conf, base_conf=BASE_CONF)
+
     # Parse group data
     # Group data are loaded in RNAseqpipe parser.
     mygroup_data = Group_data(args.group_data)
     fqnames, myfq1, myfq2 = mygroup_data.get2pip()
 
     run_salmonpip(myfq1,myfq2,fqnames,args.genome,args.gtf, args.outdir, p=args.thread)
-
-    #salmonpip(myconf,args.infile,args.outfile,args.gtf,silence=False)
-    #args=[SALMON]
-    #args.extend(argv)
-    #subprocess.call(map(str,args))
     
 if __name__ == '__main__':
 
--- test/RNAseqpip_data/run_test.sh
+++ test/RNAseqpip_data/run_test.sh
@@ -15,7 +15,7 @@ cf_root="../../confs"
 mkdir testout
 ## set2exp count mode
 ## quite mode
-#../../run_RNAseqpipe.py seq2exp -c ${cf_root}/dUTP.conf,${cf_root}/gc_no_gff.conf,${cf_root}/hisat_stringtie_verse.conf -g ${sg} -o testout/hvc_count_out -q && echo "hsv seq2exp done!" || echo "hsv seq2exp faild!"
+../../run_RNAseqpipe.py seq2exp -c ${cf_root}/dUTP.conf,${cf_root}/gc_no_gff.conf,${cf_root}/hisat_stringtie_verse.conf -g ${sg} -o testout/hvc_count_out -q && echo "hsv seq2exp done!" || echo "hsv seq2exp faild!"
 ## verbose mode
 #../../run_RNAseqpipe.py seq2exp -c ${cf_root}/dUTP.conf,${cf_root}/gc_no_gff.conf,${cf_root}/dUTP.conf,${cf_root}/hisat_stringtie_verse.conf -g ${sg} -o testout/hvc_count_out --verbose && echo "hsv seq2exp done!" || echo "hsv seq2exp faild!"
 #echo "../../run_RNAseqpipe.py seq2exp -c ${cf_root}/hisat_stringtie_verse.conf -g ${sg} -o testout/hvc_count_out --verbose" 
@@ -33,4 +33,4 @@ mkdir testout
 #../../run_RNAseqpipe.py seq2exp -c ${cf_root}/gc.conf,${cf_root}/hisat_verse_count.conf -g ${sg} -o testout/hv_out --verbose && echo "hv seq2exp done!" || echo "hcv seq2exp faild!"
 
 ## salmon
-../../run_RNAseqpipe.py salmon -g $sg --gtf data/dlmrna.gc.final.gff --genome /biodb/genomes/Fish/grass_carp/new_genome/del_empty_line.alter.C_idella_female_scaffolds.fasta -o testout/salmon
+#../../run_RNAseqpipe.py salmon -g $sg --gtf data/dlmrna.gc.final.gff --genome /biodb/genomes/Fish/grass_carp/new_genome/del_empty_line.alter.C_idella_female_scaffolds.fasta -o testout/salmon
