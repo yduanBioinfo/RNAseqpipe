be1c774292506460bed69eb5392a86a1bfb6f705
yduanBioinfo <duanyou@outlook.com>
2021-05-06 01:58:40 +0800

    Inclosure of test data.

--- test/test_Pipe.py
+++ test/test_Pipe.py
@@ -4,6 +4,7 @@ import sys, os
 import subprocess
 import filecmp
 import glob
+import pytest
 from ..run_RNAseqpipe import main as run_main
 
 gp_root="./test/RNAseqpip_data/data/gps"
@@ -27,6 +28,7 @@ def _check_exist(targets):
     for t in targets:
         assert os.path.exists(t) and os.path.getsize(t) > 100
 
+#@pytest.mark.skip(reason="no way of currently testing this")
 def test_hvc(tmpdir):
     """Hisat2 + stringtie + verse + salmon
 
@@ -39,7 +41,7 @@ def test_hvc(tmpdir):
     #assert filecmp.cmp(refdir+"/"+t,tmpdir.join(t))
     """
     #refdir = refroot+"/hvc_count_out"
-    order = "./run_RNAseqpipe.py seq2exp -c {0}/dUTP.conf,{0}/gc_no_gff.conf,{0}/hisat_stringtie_verse.conf -g {1} -o {2} ".format(cf_root,sg,tmpdir).split()
+    order = "./run_RNAseqpipe.py seq2exp -c {0}/dUTP.conf,{0}/test_gc_no_gff.conf,{0}/hisat_stringtie_verse.conf -g {1} -o {2} ".format(cf_root,sg,tmpdir).split()
     subprocess.call(order)
     
     targets = ["all_flagstat.txt","merged_asm/merged.fa","merged_asm/merged.gtf","salmon/quant_merge.elen","salmon/quant_merge.numreads","salmon/quant_merge.len","salmon/quant_merge.tpm"]
@@ -47,10 +49,11 @@ def test_hvc(tmpdir):
     _check_exist(targets)
     _check_exist(glob.glob(str(tmpdir.join("merged*.count"))))
 
+#@pytest.mark.skip(reason="no way of currently testing this")
 def test_hcv(tmpdir):
     """Hisat2 + cufflinks + verse"""
     #refdir = refroot+"/hcv_out"
-    order = "./run_RNAseqpipe.py seq2exp -c {0}/gc_no_gff.conf,{0}/hisat_cuff_verse.conf -g {1} -o {2} ".format(cf_root,sg,tmpdir).split()
+    order = "./run_RNAseqpipe.py seq2exp -c {0}/test_gc_no_gff.conf,{0}/hisat_cuff_verse.conf -g {1} -o {2} ".format(cf_root,sg,tmpdir).split()
     subprocess.call(order)
     
     targets = ["all_flagstat.txt","merged_asm/merged.fa","merged_asm/merged.gtf","salmon/quant_merge.elen","salmon/quant_merge.numreads","salmon/quant_merge.len","salmon/quant_merge.tpm"]
@@ -58,10 +61,11 @@ def test_hcv(tmpdir):
     _check_exist(targets)
     _check_exist(glob.glob(str(tmpdir.join("merged*.count"))))
 
+#@pytest.mark.skip(reason="no way of currently testing this")
 def test_hv(tmpdir):
     """Hisat2 + verse"""
     #refdir = refroot+"/hcv_out"
-    order = "./run_RNAseqpipe.py seq2exp -c {0}/gc.conf,{0}/hisat_verse_count.conf -g {1} -o {2}".format(cf_root,sg,tmpdir).split()
+    order = "./run_RNAseqpipe.py seq2exp -c {0}/test_gc.conf,{0}/hisat_verse_count.conf -g {1} -o {2}".format(cf_root,sg,tmpdir).split()
     subprocess.call(order)
     
     targets = ["all_flagstat.txt"]
@@ -69,6 +73,7 @@ def test_hv(tmpdir):
     _check_exist(targets)
     _check_exist(glob.glob(str(tmpdir.join("merged*.count"))))
 
+#@pytest.mark.skip(reason="no way of currently testing this")
 def test_salmon(tmpdir):
     """salmon"""
     #refdir = refroot+"/hcv_out"
